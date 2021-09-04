6e7984425be1a133a49cf7215b226005240dfdd5
Tharaka Rathnayake <tharaka.rathnayake@cammsgroup.com>
2021-09-05 01:44:37 +0530

    Config app and installed Sass/bootstrap packages

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return(
+        <div>
+            <h1 className="pt-1">Header</h1>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Landing = () => {
+    return(
+        <div className="row">
+            <h2>Main Content</h2>
+        </div>
+    )
+}
+
+export default Landing
