e3485597e3deb9822697d73a90ad111d562a36ca
Tharaka Rathnayake <tharaka.rathnayake@cammsgroup.com>
2021-10-12 00:43:44 +0530

    Initiate home page carousel for main events

---
+++ src/components/HomeCarousel.js
@@ -0,0 +1,113 @@
+import React, { Component } from "react";
+import RBCarousel from "react-bootstrap-carousel";
+import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
+import sImageURL1 from '../images/ig-4.jpg';
+import sImageURL2 from '../images/ig-2.jpg';
+import sImageURL3 from '../images/ig-3.jpg';
+
+const sImage1 = {
+    backgroundImage: `url(${sImageURL1})`,
+}
+const sImage2 = {
+    backgroundImage: `url(${sImageURL2})`,
+}
+const sImage3 = {
+    backgroundImage: `url(${sImageURL3})`,
+}
+
+class HomeCarosel extends Component {
+    constructor(props) {
+        super(props);
+        // this.slider = React.createRef();
+        this.state = {
+            autoplay: false,
+        };
+    }
+
+    render() {      
+        return (
+            <div className="h-100">
+                <RBCarousel
+                    animation={true}
+                    autoplay={this.state.autoplay}
+                    slideshowSpeed={1000}
+                    defaultActiveIndex={0}
+                    // leftIcon={this.state.leftIcon}
+                    // rightIcon={this.state.rightIcon}
+                    // onSelect={this._onSelect}
+                    // ref={this.slider}
+                    version={4}
+                >
+                    <div className="h-100">
+                        <div className="row h-50">
+                            <div className="col-4 slider-tile tile-bg-1">
+                                <h4><a href="#">AGM Meetings</a></h4>
+                                <p>Aliquam dapibus tincidunt metus. Praesent justo dolor, lobortis quis, lobortis dignissim,
+                                     pulvinar ac, lorem. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vestibulum...</p>
+                                <a href="#">Read More</a>     
+                            </div>
+                            <div className="col-4 p-0">
+                                <a href="#"><div className="s-image-wrapper" style={sImage1}></div></a>
+                            </div>
+                            <div className="col-4 slider-tile tile-bg-3">
+                            <h4><a href="#">NEW CLASSROOM TECHNOLOGY</a></h4>
+                                <p>Aliquam dapibus tincidunt metus. Praesent justo dolor, lobortis quis, lobortis dignissim,
+                                     pulvinar ac, lorem. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vestibulum...</p>
+                                <a href="#">Read More</a>
+                            </div>
+                        </div>
+                        <div className="row h-50">
+                            <div className="col-4 p-0">
+                                <a href="#"><div className="s-image-wrapper" style={sImage2}></div></a>
+                            </div>
+                            <div className="col-4 slider-tile tile-bg-4">
+                                <h4><a href="#">NEW CLASSROOM TECHNOLOGY</a></h4>
+                                <p>Aliquam dapibus tincidunt metus. Praesent justo dolor, lobortis quis, lobortis dignissim,
+                                     pulvinar ac, lorem. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vestibulum...</p>
+                                <a href="#">Read More</a>
+                            </div>
+                            <div className="col-4 p-0">                                
+                                <a href="#"><div className="s-image-wrapper" style={sImage3}></div></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="h-100">
+                        <div className="row h-50">
+                            <div className="col-4 slider-tile tile-bg-1">
+                                <h4><a href="#">AGM Meetings</a></h4>
+                                <p>Aliquam dapibus tincidunt metus. Praesent justo dolor, lobortis quis, lobortis dignissim,
+                                     pulvinar ac, lorem. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vestibulum...</p>
+                                <a href="#">Read More</a>     
+                            </div>
+                            <div className="col-4 p-0">
+                                <a href="#"><div className="s-image-wrapper" style={sImage1}></div></a>
+                            </div>
+                            <div className="col-4 slider-tile tile-bg-3">
+                            <h4><a href="#">NEW CLASSROOM TECHNOLOGY</a></h4>
+                                <p>Aliquam dapibus tincidunt metus. Praesent justo dolor, lobortis quis, lobortis dignissim,
+                                     pulvinar ac, lorem. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vestibulum...</p>
+                                <a href="#">Read More</a>
+                            </div>
+                        </div>
+                        <div className="row h-50">
+                            <div className="col-4 p-0">
+                                <a href="#"><div className="s-image-wrapper" style={sImage2}></div></a>
+                            </div>
+                            <div className="col-4 slider-tile tile-bg-4">
+                                <h4><a href="#">NEW CLASSROOM TECHNOLOGY</a></h4>
+                                <p>Aliquam dapibus tincidunt metus. Praesent justo dolor, lobortis quis, lobortis dignissim,
+                                     pulvinar ac, lorem. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vestibulum...</p>
+                                <a href="#">Read More</a>
+                            </div>
+                            <div className="col-4 p-0">                                
+                                <a href="#"><div className="s-image-wrapper" style={sImage3}></div></a>
+                            </div>
+                        </div>
+                    </div>
+                </RBCarousel>
+            </div>
+        )
+    }
+}
+
+export default HomeCarosel;
--- src/components/SecImageGallery.js
+++ src/components/SecImageGallery.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import HomeCarosel from './HomeCarousel';
 
 const SecImageGallery = () => {
     return (
         <section className="image-gallery-section">
-            <div className="container">
-                <div>
+            <div className="container animate-gallery">
+                <div className="gallery-title">
                     <h1 className="topTitle">What we have done</h1>
                 </div>
-                <div>
-                    
+                <div className="carousel-wrapper">
+                    <HomeCarosel />
                 </div>
             </div>            
         </section>
